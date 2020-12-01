1eea9e88ec02cf3c76b5a4aed0089a311153cefb
bluetoonist <bluetoon@naver.com>
2020-12-02 01:40:11 +0900

    [+] Data Reverse print

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import MentionBoard from "./layouts/MentionBoard.js"
+
 import MentionCrt from "./layouts/MentionCrt.js"
 function App() {
   return (
     <>
       <MentionCrt/>
-      {/* <MentionBoard/> */}
+  
     </>
 
     );
--- src/layouts/MentionCrt.js
+++ src/layouts/MentionCrt.js
@@ -1,7 +1,14 @@
-import React from 'react';
+/* 
+@ author : jak010
+@ Description : 왼쪽에 나타나는 레이아웃
 
+*/
+import React,{createContext,useState} from 'react';
 import { makeStyles } from "@material-ui/core/styles"
-import { Grid, Box, Container, TextField } from "@material-ui/core"
+import { Grid, Box, Container, TextField,Button } from "@material-ui/core"
+
+export const userText =  createContext()
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,17 +32,35 @@ const useStyles = makeStyles((theme) => ({
 
 
 
+
 export default function MentionCrt() {
+
+    const [saveData,setsaveData] = useState([]);
+    const revDataSet = Array();
+
     const classes = useStyles();
+    const getTextDate = (e) => {
+        e.preventDefault();
+        
+        const usertext = e.target[0].value;
+        console.log(usertext)
+        
+        // setsaveData([...saveData,usertext])
+        revDataSet.push(userText);
+        console.log(revDataSet)
+    }
+
+
 
     return (
+        // Mention Create BOx 
         <Grid container spacing={1} className={classes.root}>
 
             <Grid item xs={12} sm={4} className={classes.mentioncrt}>
                 <Box bgcolor="info.main" color="info.contrastText" p={1}>
-                    Metion Create
+                    MENTION POST IT!
           </Box>
-                <form className={classes.root} noValidate autoComplete="off">
+                <form className={classes.root} noValidate autoComplete="off" onSubmit={getTextDate}>
                     <TextField
 
                         id="standard-full-width"
@@ -47,20 +72,18 @@ export default function MentionCrt() {
                         margin="normal"
                         fullWidth
                     />
+                 <input type="submit" value="Submit"/>  
                 </form>
-
+               
             </Grid>
 
+            {/* Mention Monitor Layout */}
             <Grid item xs={12} sm={7} >
-
                 <Container maxWidth="100%" className={classes.mentionbrd}>
-                    <TextField id="outlined-basic" className={classes.mentionbox}  rows={12} label="Outlined" variant="outlined" fullWidth/>
-                    <TextField id="outlined-basic"  className={classes.mentionbox} rows={12} label="Outlined" variant="outlined" fullWidth/>
-                    <TextField id="outlined-basic"  className={classes.mentionbox} rows={12} label="Outlined" variant="outlined" fullWidth/>
-
-
+                    {revDataSet.map (index => (              
+                    <TextField id="outlined-basic" className={classes.mentionbox}  rows={12} label={index} variant="outlined" fullWidth/>     
+                    ))}         
                 </Container>
-
             </Grid>
 
 
