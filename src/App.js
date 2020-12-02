94e8bdc7177f51c74528a091cd955868c7ab1821
bluetoonist <bluetoon@naver.com>
2020-12-03 00:35:35 +0900

    [U] Two Column Layout Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React,{useState} from 'react';
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 import MentionCrt from "./layouts/MentionCrt.js"
+import MentionBrd from "./layouts/MentionBrd.js"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    height : "100%",
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
+  const [userText, setuserText] = useState([])
+
   return (
     <>
-      <MentionCrt/>
-  
+    <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <MentionCrt setData={setuserText}/>
+        
+        </Grid>
+      
+        <Grid item xs={6}>
+        
+          <MentionBrd useData={userText}/>
+        </Grid>
+      
+      </Grid>
     </>
 
     );
--- src/layouts/MentionCrt.js
+++ src/layouts/MentionCrt.js
@@ -7,7 +7,7 @@ import React,{createContext,useState} from 'react';
 import { makeStyles } from "@material-ui/core/styles"
 import { Grid, Box, Container, TextField,Button } from "@material-ui/core"
 
-export const userText =  createContext()
+export const userContext =  createContext()
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,11 +19,7 @@ const useStyles = makeStyles((theme) => ({
         marginRight: "2%",
         marginLeft: "0.5%"
     },
-    mentionbrd: {
-        height: "100%",
-        marginLeft: "1%",
-        background: "#fff5f2"
-    },
+
     mentionbox : {
         marginTop:"2%"
     }
@@ -33,60 +29,43 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-export default function MentionCrt() {
+export default function MentionCrt(props) {
 
     const [saveData,setsaveData] = useState([]);
-    const revDataSet = Array();
 
     const classes = useStyles();
+     
     const getTextDate = (e) => {
-        e.preventDefault();
-        
+        e.preventDefault();        
         const usertext = e.target[0].value;
-        console.log(usertext)
+        setsaveData([...saveData,usertext])
+        props.setData(saveData)
         
-        // setsaveData([...saveData,usertext])
-        revDataSet.push(userText);
-        console.log(revDataSet)
     }
-
-
 
     return (
         // Mention Create BOx 
         <Grid container spacing={1} className={classes.root}>
 
-            <Grid item xs={12} sm={4} className={classes.mentioncrt}>
+            <Grid item xs={12} sm={12} className={classes.mentioncrt}>
                 <Box bgcolor="info.main" color="info.contrastText" p={1}>
                     MENTION POST IT!
-          </Box>
+                 </Box>
                 <form className={classes.root} noValidate autoComplete="off" onSubmit={getTextDate}>
                     <TextField
 
                         id="standard-full-width"
                         label="Hello Input Your Mention !"
                         multiline
-                        rows={12}
+                        rows={6}
                         defaultValue="Default Value"
                         variant="outlined"
                         margin="normal"
                         fullWidth
                     />
-                 <input type="submit" value="Submit"/>  
+                    <input type="submit" value="Submit"/>  
                 </form>
-               
             </Grid>
-
-            {/* Mention Monitor Layout */}
-            <Grid item xs={12} sm={7} >
-                <Container maxWidth="100%" className={classes.mentionbrd}>
-                    {revDataSet.map (index => (              
-                    <TextField id="outlined-basic" className={classes.mentionbox}  rows={12} label={index} variant="outlined" fullWidth/>     
-                    ))}         
-                </Container>
-            </Grid>
-
-
         </Grid>
     );
 }
